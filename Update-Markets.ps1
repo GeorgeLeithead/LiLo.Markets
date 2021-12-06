e2ad3d62a3ef8f471e7ad831c8f2ccd5ec2b8370
github@internetwideworld.com <github@internetwideworld.com>
2021-12-06 14:10:17 +0000

    Updated

--- Update-Markets.ps1
+++ Update-Markets.ps1
@@ -1,36 +1,46 @@
 Import-Module posh-git
 git pull origin main
+[bool]$CommitNeeded = $false
 
 # Get the feeds
 Write-Host "Getting CoinGeco feed..."
 $Markets = Invoke-RestMethod "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=150&page=1&sparkline=false"
-Write-Host "Getting local markets.json"
-$Lilo = Get-Content -path .\Markets.json | ConvertFrom-Json
-
-# IOTA has a weird symbol on CoinGeko, so lets match
-$Miota = $Lilo.Markets | Where-Object {$_.SymbolString -eq "iota"}
-$Miota.SymbolString = "MIOTA"
-
-$matchingMarkets = $Markets | Where-Object {$_.symbol -in $Lilo.Markets.SymbolString }
-[bool]$Changes = $false
-foreach ($Market in $matchingMarkets)
+$MarketFiles = Get-ChildItem -Path .\ -File -Filter *.json
+foreach($MarketFile in $MarketFiles)
 {
-    $LiloItem = $Lilo.Markets | Where-Object {$_.SymbolString -eq $Market.symbol}
-    if ($LiloItem.Rank -ne $Market.market_cap_rank)
+    [bool]$Changes = $false
+    Write-Host "*** Getting local $($MarketFile.Name) ***"
+    $Lilo = Get-Content -path $MarketFile | ConvertFrom-Json
+
+    # IOTA has a weird symbol on CoinGeko, so lets match
+    $Miota = $Lilo.Markets | Where-Object {$_.SymbolString -eq "iota"}
+    $Miota.SymbolString = "MIOTA"
+
+    $matchingMarkets = $Markets | Where-Object {$_.symbol -in $Lilo.Markets.SymbolString }
+    foreach ($Market in $matchingMarkets)
     {
-        $Changes = $true
-        write-host "$($LiloItem.SymbolString) - Rank: $($LiloItem.Rank) now $($Market.market_cap_rank)"
-        $LiloItem.Rank = $Market.market_cap_rank
+        $LiloItem = $Lilo.Markets | Where-Object {$_.SymbolString -eq $Market.symbol}
+        if ($LiloItem.Rank -ne $Market.market_cap_rank)
+        {
+            $Changes = $true
+            $CommitNeeded = $true
+            write-host "$($LiloItem.SymbolString) - Rank: $($LiloItem.Rank) now $($Market.market_cap_rank)"
+            $LiloItem.Rank = $Market.market_cap_rank
+        }
+    }
+
+    if ($Changes)
+    {
+        # Undo the IOTA change
+        $Miota = $Lilo.Markets | Where-Object {$_.SymbolString -eq "miota"}
+        $Miota.SymbolString = "IOTA"
+        $Lilo | ConvertTo-Json | Out-File $MarketFile
     }
 }
 
-if ($Changes)
+if ($CommitNeeded)
 {
-    # Undo the IOTA change
-    $Miota = $Lilo.Markets | Where-Object {$_.SymbolString -eq "miota"}
-    $Miota.SymbolString = "IOTA"
-    $Lilo | ConvertTo-Json | Out-File .\Markets.json
-    Write-Host "UPDATING Markets.json"
+    Write-Host "*** Market Repository ***"
     git commit -m "Updated" -a
     git push
 }
