ebbecbeadb203460a8d5176b157e5550c781ff5a
github@internetwideworld.com <github@internetwideworld.com>
2022-02-16 15:54:27 +0000

    New script to identify and get details of missing currencies, and match to CoinGeco and Binance. Added new crypto markets where missing and icon found.

---
+++ Get-MissingMarkets.ps1
@@ -0,0 +1,53 @@
+$Markets = Invoke-RestMethod "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=300&page=1&sparkline=false"
+$Lilo = Get-Content -path .\Markets1.3.json | ConvertFrom-Json
+$SymbolsToIgnore = @(
+	"usdt", # Stable Coin
+	"usdc", # Stable coin
+	"miota", # AKA IOTA
+	"cro", # Not on binance
+)
+$MissingMarketsInLiLo = @()
+foreach($Market in $Markets)
+{
+	if ($Lilo.Markets.SymbolString -notcontains $Market.symbol)
+	{
+		if ($SymbolsToIgnore -notcontains $($Market.Symbol))
+		{
+			$MissingMarketsInLiLo += $Market
+		}
+	}
+}
+
+Write-Host $Market
+
+$BinanceMarkets = Invoke-RestMethod "https://api.binance.com/api/v3/exchangeInfo"
+$BinanceMarketsUsdt = $BinanceMarkets.symbols | Where-Object quoteAsset -eq "USDT" | Where-Object status -eq "TRADING" | Where-Object permissions -contains "SPOT"
+$MissingFromLiLo = @()
+
+foreach($BinanceSymbol in $BinanceMarketsUsdt)
+{
+	[string]$Symbol = $BinanceSymbol.baseAsset.ToLower()
+	if ($MissingMarketsInLiLo.symbol -contains $Symbol)
+	{
+		$Tick = $BinanceSymbol | Select-Object @{n = "tickSize"; e = { $_.filters.tickSize[0]}}
+		[int]$TickSize = $Tick.tickSize.split('1')[0].Length -1
+		if ($TickSize -lt 0)
+		{
+			$TickSize = 0
+		}
+
+		$Rank = $Markets | Where-Object {$_.symbol -eq $Symbol} | select market_cap_rank
+
+		$NewMarket = [PsCustomObject]@{
+			SymbolString = $Symbol.ToUpper()
+			DecimalPlaces = $TickSize
+			Rank = $Rank.market_cap_rank
+		}
+		$MissingFromLiLo += $NewMarket
+	}
+}
+
+$SymbolsWithIcons = @('TUSD', 'GNO', 'LPT', 'ANKR', 'BTG', 'SYS', 'SKL', 'POLY', 'PAXG', 'FLUX', 'DENT', 'LSK', 'POWR')
+
+# This is the JSON to add to Markets.json
+$MissingFromLiLo | Where-Object SymbolString -in $SymbolsWithIcons | Sort-Object Rank | ConvertTo-Json
